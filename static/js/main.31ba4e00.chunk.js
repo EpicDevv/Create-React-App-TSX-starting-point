7781deff05778d84e864e1b5b6c5365a6fd26836
Meow Security <meowsecurity@Meows-MacBook-Pro.local>
2021-09-12 03:30:35 -0700

    Updates

---
+++ static/js/main.31ba4e00.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);var c=n(1),s=n.n(c),i=n(3),o=n.n(i),r=n(0);function a(){return Object(r.jsxs)("div",{className:"App",children:[Object(r.jsx)("h1",{children:"hello code sandbox"}),Object(r.jsx)("h2",{children:"start editing to see some magic happen"})]})}n(9);o.a.render(Object(r.jsx)(s.a.StrictMode,{children:Object(r.jsx)(a,{})}),document.getElementById("root"))},9:function(e,t,n){}},[[10,1,2]]]);
+//# sourceMappingURL=main.31ba4e00.chunk.js.map
