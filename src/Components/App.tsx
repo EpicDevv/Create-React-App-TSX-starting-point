9e6ac3965a37537658c0a6d3d245ac42d589377c
Meow Security <meowsecurity@Meows-MacBook-Pro.local>
2021-09-12 03:27:35 -0700

    Adding To Repo

---
+++ src/Components/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function App() {
+    return (
+        <div className="App">
+            <h1>hello code sandbox</h1>
+            <h2>start editing to see some magic happen</h2>
+
+        </div>
+    )
+}
